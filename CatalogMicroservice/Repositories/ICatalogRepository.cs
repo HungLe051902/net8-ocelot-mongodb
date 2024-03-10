8d76c03a81ebd700559f3ffd5b390ba726317475
HungLe <lexuanhung020599@gmail.com>
2024-03-10 22:57:12 +0700

    Add project files.

---
+++ CatalogMicroservice/Repositories/ICatalogRepository.cs
@@ -0,0 +1,14 @@
+﻿using CatalogMicroservice.Models;
+
+namespace CatalogMicroservice.Repositories
+{
+    public interface ICatalogRepository
+    {
+        IList<CatalogItem> GetCatalogItems();
+        CatalogItem? GetCatalogItem(string catalogItemId);
+        void InsertCatalogItem(CatalogItem catalogItem);
+        void UpdateCatalogItem(CatalogItem catalogItem);
+        void DeleteCatalogItem(string catagItemId);
+
+    }
+}
